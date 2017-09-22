04859ad4c82f5df80d2a2b74454a34ecbf3dbbe8
jgerminario <jgerminario@gmail.com>
2017-09-22 12:40:12 -0700

    Master: finalizes site

--- partials/cip-contact-form.php
+++ partials/cip-contact-form.php
@@ -1,5 +1,5 @@
 <?php
   echo do_shortcode(
-    "[contact-form to='tjgerminario@earthlink.net; jgerminario@gmail.com' subject='New patent inquiry from website'][contact-field label='Name *' type='name' required='1'/][contact-field label='Phone *' type='text' required='1'/][contact-field label='Email *' type='email' required='1'/][contact-field label='Best time to call?' type='text'/][contact-field label='Comment' type='textarea'/][/contact-form]"
+    "[contact-form to='tjgerminario@earthlink.net; jgerminario@gmail.com' subject='New patent inquiry from website'][contact-field label='Name*' type='name' required='1'/][contact-field label='Phone*' type='text' required='1'/][contact-field label='Email*' type='email' required='1'/][contact-field label='Best time to call?' type='text'/][contact-field label='Comment' type='textarea'/][/contact-form]"
   );
 ?>
