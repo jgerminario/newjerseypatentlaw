3369037cdf620abe5facb8d7d4cdeba9ebe39eed
jgerminario <jgerminario@gmail.com>
2017-09-22 13:43:04 -0700

    Master: replaces exclude with include for footer display

--- partials/cip-footer.php
+++ partials/cip-footer.php
@@ -1,4 +1,4 @@
 <div id="footer">
-  <?php wp_list_pages( array('title_li' => '', 'exclude'=>'20')); ?> 
+  <?php wp_list_pages( array('title_li' => '', 'include'=>array(10, 45, 23, 20, 27, 64, 67, 58, 69, 72, 74, 62, 209, 25, 78, 83, 80, 85, 87, 9))); ?> 
   <p class="footer">©<?php echo date("Y"); ?> The Law Office of Thomas J Germinario</p>
 </div>
